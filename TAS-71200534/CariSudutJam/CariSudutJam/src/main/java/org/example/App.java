a05dc839eea6cf8a7d393fcefc14cb790e39d501
mikhaelryan <80173365+mikhaelryan@users.noreply.github.com>
2022-06-20 17:55:18 +0700

    Add files via upload

--- TAS-71200534/CariSudutJam/CariSudutJam/src/main/java/org/example/App.java
+++ TAS-71200534/CariSudutJam/CariSudutJam/src/main/java/org/example/App.java
@@ -1,6 +1,5 @@
 package org.example;
 
-import javax.swing.plaf.nimbus.State;
 import java.sql.*;
 
 public class App 
@@ -8,7 +7,7 @@ public class App
     public double sudut(String s) {
         String[] arr=s.split(":");
         int jam= Integer.parseInt(arr[0]);
-        int menit= Integer.parseInt(arr[1]);
+        int menit= Integer.parseInt((arr[1]));
 
         double derajat=0.5*(jam*60+menit);
         double derajat2=6*menit;
@@ -34,16 +33,16 @@ public class App
     public void createNewTable() {
     String sql = "create table IF NOT EXISTS waktu" +
             "(\n" +
-            " nomor int not null\n" +
+            " nomor integer not null\n" +
             " constraint waktu_pk\n" +
             " primary key autoincrement,\n" +
             " waktu text not null,\n" +
             " sudut real null\n" +
             ");";
-    String sql2="INSERT INTO waktu values('','15:00',''),('','07:30',''),\n" +
-            " ('','08:05',''),\n" +
-            " ('','17:35',''),\n" +
-            " ('','11:01','');";
+    String sql2="INSERT INTO waktu values('1','15:00','2'),('2','07:30','2'),\n" +
+            " ('3','08:05','2'),\n" +
+            " ('4','17:35','2'),\n" +
+            " ('5','11:01','2');";
     try(Connection conn= DriverManager.getConnection(url)){
         Statement stm = conn.createStatement();
         stm.execute(sql);
@@ -59,8 +58,10 @@ public class App
             Statement stm = conn.createStatement();
             ResultSet rs=stm.executeQuery(sql);
             while (rs.next()){
-                int hasil= (int) sudut(rs.getString("sudut"));
+                double hasil= sudut(rs.getString("sudut"));
                 String sql2=  "Update waktu set sudut="+hasil+" WHere nomor="+rs.getString("nomor");
+                Statement stm2 = conn.createStatement();
+                stm2.execute(sql2);
             }
         }catch (SQLException e){
             System.out.println(e.getMessage());
